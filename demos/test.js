688cdb45709153f04c6aeed758f1aaa84dde0e41
Michael <Iamg1975>
2020-07-02 00:55:07 -0400

    Contour

--- demos/test.js
+++ demos/test.js
@@ -5,12 +5,17 @@ const EDO = require("../edo").EDO
 const Scale = require("../edo").Scale
 
 let edo = new EDO(12) //Create a tuning context
-// let scale = edo.scale([0,2,4,7,9]) //define new scale
-let scale = edo.scale([0,2,4,5,7,9,11]) //define new scale
-// let scale = edo.scale([0,4,7]) //Major triad
+let scale = edo.scale([0,2,4,7,9]) //define new scale
+// let scale = edo.scale([0,2,4,5,7,9,11]) //define new scale
+// console.log(edo.get.normal_order([0,18,2,3]))
+// console.log(edo.get.random_melody(4, [-3, 2])); //returns e.g. [ -2, -1, 1, 2 ]
+// console.log(edo.get.random_melody(4, [-3, 2],1)); //returns e.g. [ 2, 1, -3, -2 ]
+// console.log(edo.get.random_melody(6, [0, 17], 6,[0, 2, 4, 5, 7, 9, 11])); // returns e.g. [ 7, 9, 2, 17, 4, 4 ]
+console.log(scale.get.sequence_transposition([0,2,4],1))
 
-let time1 = Date.now()
-console.log(scale.get.n_chords(7,true),Date.now()-time1)
+
+
+
 
 
 
